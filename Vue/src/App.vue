2b854d1c082216ef86f81c3af86c9fa07ff5129d
Phil Knowles <fighters4christ@outlook.com>
2023-06-05 19:27:57 -0500

    Adding Ag-Grid

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <div>
     <Managements />
+    <AgGrid />
   </div>
 </template>
 
 <script setup>
 //import Posts from './components/Post.vue'
   import Managements from './components/Management.vue'
+  import AgGrid from './components/AgGrid.vue'
 </script>
 
 
